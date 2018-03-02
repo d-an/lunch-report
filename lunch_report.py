dd1fd6d744a2cf279f8d3b0f4dd604ff76f9151f
d-an <dv.daniel@centrum.cz>
2018-03-02 17:03:35 +0100

    add try..except where necessary; report broken items at the end

--- lunch_report.py
+++ lunch_report.py
@@ -6,20 +6,20 @@ from pyperclip import copy
 # user key: 
 with open('../key.txt', 'rt') as f:
     user_key = f.read()
-    
+
 url_base = "https://developers.zomato.com/api/v2.1/"
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": user_key}
 
 ids = {"let's meat": 18494543,
-  # "pizza u staré pece": 16512835 ,
-  "olše": 16513849,
-  "u vodoucha":  16506331,
-  "sonora":  16506702,
-  "incruenti": 18355481,
-  # "u vodárny": 16506550,
-  "hospůdka v ateliéru": 16506560,
-  "nominanza": 16513818, 
-  "vinohradský pivovar": 16507624}
+       # "pizza u staré pece": 16512835 ,
+       "olše": 16513849,
+       "u vodoucha": 16506331,
+       "sonora": 16506702,
+       "incruenti": 18355481,
+       # "u vodárny": 16506550,
+       "hospůdka v ateliéru": 16506560,
+       "nominanza": 16513818,
+       "vinohradský pivovar": 16507624}
 
 
 def ask(query):
@@ -33,15 +33,15 @@ def menu(res_id):
     dishes = [(dish['dish']['name'], dish['dish']['price']) for dish in dishes]
     # filter out empty price tags:
     dishes = [(name, price) for (name, price) in dishes if price]
-    # uprav nazvy a ceny:
+    # modify names and prices:
     dishes = [(name.strip(), price.split('\xa0')[0]) for (name, price) in dishes]
     return dishes
 
 
-# jednotliva menu pujdou do slovniku:
-data = dict()    
-    
-for (k,v) in ids.items():
+# menus will go to this dictionary:
+data = dict()
+
+for (k, v) in ids.items():
     time.sleep(1)
     try:
         data[k] = menu(v)
@@ -49,38 +49,49 @@ for (k,v) in ids.items():
         pass
 
 
-        
-# restaurace, ktere nepokryva API:
-
+# restaurants not available through zomato:
 import parsers as p
 
-data['Infinity'] = p.Infinity(p.res_links['Infinity']).get_menu().name_price_pairs
-# data['Prasatka'] = p.Prasatka(p.res_links['Prasatka']).get_menu().name_price_pairs 
-# data['Fratello'] = p.Fratello(p.res_links['Fratello']).get_menu().name_price_pairs 
-# data['Bila vrana'] = p.Vrana(p.res_links['Vrana']).get_menu().name_price_pairs 
-data['Happy Bean'] = p.HappyBean(p.res_links['Happy Bean']).get_menu().name_price_pairs 
-	
-	
-# setrid restaurace:
-poradi = [# "incruenti",
-          "Happy Bean", 
-          "nominanza", 
+others = {'Infinity': p.Infinity,
+          'Prasatka': p.Prasatka,
+          'Fratello': p.Fratello,
+          'Bila vrana': p.Vrana,
+          'Happy Bean': p.HappyBean
+          }
+
+broken = []
+
+for name, parser in others.items():
+    try:
+        data[name] = parser(p.res_links[name]).get_menu().name_price_pairs
+    except Exception:
+        broken.append(name)
+
+# sort the restaurants:
+poradi = ["incruenti",
+          "Happy Bean",
+          "nominanza",
+          "Fratello",
           "let's meat",
-          "Infinity", 
-          # "Bila vrana", 
-         # "Prasatka",
+          "Infinity",
+          "Bila vrana",
+          "Prasatka",
           "sonora",
-          "olše", 
-          "u vodoucha", 
-          # "u vodárny", 
-          "hospůdka v ateliéru", 
-          "vinohradský pivovar"] 
-          
+          "olše",
+          "u vodoucha",
+          "u vodárny",
+          "hospůdka v ateliéru",
+          "vinohradský pivovar"]
+
+# include only those that we got in data:
+poradi = [item for item in poradi if item in data]
+
+# choose the order:
 data_ordered = OrderedDict()
 
 for item in poradi:
     data_ordered[item] = data[item]
-    
+
 # vypis nekam data:
 with open('lunch_report.txt', 'wt') as report:
     for (place, dishes) in data_ordered.items():
@@ -93,8 +104,9 @@ with open('lunch_report.txt', 'wt') as report:
             except Exception:
                 report.write('...\n')
         report.write('\n\n\n\n')
-        
-# vysledek do clipboardu:
+    for item in broken:
+        report.write(item + '\n')
+
+# results to clipboard:
 with open('lunch_report.txt', 'rt') as report:
     copy(report.read())
-    
--- parsers.py
+++ parsers.py
@@ -4,88 +4,78 @@ from requests import get
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-
-res_links = {'Infinity': 'http://www.infinitybar.cz/restaurace/poledni-menu', 
-			 'Prasatka': 'http://www.triprasatka.cz/home-page/lunch/', 
-			 'Fratello': 'http://www.ristorantefratello.cz/poledni-menu/', 
+res_links = {'Infinity': 'http://www.infinitybar.cz/restaurace/poledni-menu',
+             'Prasatka': 'http://www.triprasatka.cz/home-page/lunch/',
+             'Fratello': 'http://www.ristorantefratello.cz/poledni-menu/',
              'Vrana': 'http://www.bilavrana.com/cs/poledni-menu',
              'Happy Bean': 'http://www.happybean.cz/#dennimenu'}
 
+
 class Restaurant:
+    def __init__(self, link):
+        self.page = BeautifulSoup(get(link).content.decode('utf-8'), "lxml")
 
-	def __init__(self, link):
-		self.page = BeautifulSoup(get(link).content.decode('utf-8'))
-
-	def get_menu(self):
-		pass
+    def get_menu(self):
+        pass
 
 
-        
 class Infinity(Restaurant):
-    
     def process_pair(self, pair):
-        if len(pair)==2:
-            return (pair[0].strip(), pair[1].split(']')[-1].strip())
+        if len(pair) == 2:
+            return pair[0].strip(), pair[1].split(']')[-1].strip()
         else:
-            return (pair[0].replace('\n', '').strip(), '')
-        
+            return pair[0].replace('\n', '').strip(), ''
+
     def get_menu(self):
         day_num = datetime.now().weekday() + 1
         tables = self.page.find_all('table')
         whole_week = tables[2]
-        todays_menu = tables[2+day_num]
+        todays_menu = tables[2 + day_num]
         rows = whole_week.find_all('tr')
         rows.extend(todays_menu.find_all('tr'))
         name_price_pairs = [item.text.split('\t') for item in rows]
         self.name_price_pairs = [self.process_pair(pair) for pair in name_price_pairs]
         return self
 
-        
-        
 
 class Prasatka(Restaurant):
+    def get_menu(self):
+        hlavni_jidla = self.page.find_all('ul', class_='menu-list__items')[2]
+        names = [item.find(class_='item_title').contents[0] for item in hlavni_jidla.find_all('li')]
+        prices = [item.find(class_='menu-list__item-price').contents[0] for item in hlavni_jidla.find_all('li')]
+        if len(names) != len(prices):
+            prices = ['' for item in names]
+        self.name_price_pairs = list(zip(names, prices))
+        return self
 
-	def get_menu(self):
-		hlavni_jidla = self.page.find_all('ul', class_='menu-list__items')[2]
-		names = [item.find(class_='item_title').contents[0] for item in hlavni_jidla.find_all('li')]
-		prices = [item.find(class_='menu-list__item-price').contents[0] for item in hlavni_jidla.find_all('li')]
-		if len(names) != len(prices):
-			prices = ['' for item in names]
-		self.name_price_pairs = list(zip(names, prices))
-		return self
-			
 
-            
 # a class useful for Fratello:           
 class Foods:
     def __init__(self):
         self.foods = []
-        
+
     def add_day(self):
         self.foods.append([])
-    
+
     def add_food(self, food):
         if self.foods:
-            self.foods[-1].append(food)            
+            self.foods[-1].append(food)
 
-            
-			
-            
+
 class Fratello(Restaurant):
-
     def name_price(row):
         items = row.split('…')
-        if len(items)==1:
+        if len(items) == 1:
             items = [items[0], '']
         return items
 
     def get_menu(self):
         ps = self.page.find_all('p')
         ps = [item.text.strip() for item in ps]
-        ps = [item for item in ps if item]     
+        ps = [item for item in ps if item]
         ps2 = [item.split(' ')[0] for item in ps]
-        foods = Foods()            
-        days = ['PONDĚLÍ', 'ÚTERÝ', 'STŘEDA', 'ČTVRTEK', 'PÁTEK']                        
+        foods = Foods()
+        days = ['PONDĚLÍ', 'ÚTERÝ', 'STŘEDA', 'ČTVRTEK', 'PÁTEK']
         for num, word in enumerate(ps2):
             if word in days:
                 foods.add_day()
@@ -96,23 +86,17 @@ class Fratello(Restaurant):
         todays_menu = weeks_menus[days[weekday]]
         self.name_price_pairs = [Fratello.name_price(item) for item in todays_menu]
         return self
-        
 
-        
 
 class Vrana(Restaurant):
-
     def get_menu(self):
         tds = self.page.find_all('table')[0].find_all('td')
         tds = [td.text for td in tds if td.text]
         self.name_price_pairs = [(td, '') for td in tds]
         return self
-        
-        
-        
-        
-class HappyBean(Restaurant):
 
+
+class HappyBean(Restaurant):
     def get_menu(self):
         spans = self.page.find_all('span')
         # the number of the span where daily menus start:
@@ -142,6 +126,3 @@ class HappyBean(Restaurant):
         todays_menu = list(set(todays_menu))
         self.name_price_pairs = [(food, '') for food in todays_menu]
         return self
-        
-        
-		
