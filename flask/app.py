2d48b54a8e6ff1b950d23ba20f1ec5621be4a64c
d-an <dv.daniel@centrum.cz>
2019-05-27 20:56:26 +0200

    add a flask app

---
+++ flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return 'Flask Dockerized ' + os.environ['user_key']
+
+@app.route('/')
+def show_menu():
+    with open('lunch-report/lunch_report.txt', 'rt') as f:
+        menu = f.read()
+    return menu
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
